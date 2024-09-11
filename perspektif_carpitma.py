737d6da1fd0b8325a0942e5321fc6be36c99cb4f
nabi-ozturk <nabiozturk34@gmail.com>
2024-09-11 14:03:46 +0300

    Perspektif Çarpıtma
    Python' la görüntü işlemede perspektif çalışma üzerinde durduk.

---
+++ perspektif_carpitma.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+# içe aktar resim
+img = cv2.imread("1_XxL3dg8h4WtAqDkothVM2g.png")
+cv2.imshow("Orijinal", img)
+
+width = 400
+height = 500
+
+pts1 = np.float32([[203,1],[1,472],[540,150],[338,617]])
+pts2 = np.float32([[0,0],[0,height],[width,0],[width,height]])
+
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+print(matrix)
+
+# nihai dönüştürülmüş resim
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+cv2.imshow("Nihai Resim", imgOutput)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
